dd6aa3015887d756e234d47b015d77b9659fe8be
Mohammed Amin <mdamin67541@gmail.com>
2026-06-14 20:56:22 +0530

    Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
-import { Analytics } from "@vercel/analytics/react";
+import { Analytics } from "@vercel/analytics/react"; // Importing Analytics
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
